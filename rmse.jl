27a9ea78fc010bd07f0239394dd12f34e8d337ea
Abdelrahman95 <abdelrahmanfathe95@gmail.com>
2024-07-02 21:27:57 +0200

    fix nvtx annotation

--- rmse.jl
+++ rmse.jl
@@ -8,11 +8,12 @@ N = 16
 A = CUDA.rand(1024, 1024, N)
 B = CUDA.rand(1024, 1024, N)
 
-NVTX.@range function rmse(A::AbstractMatrix, B::AbstractMatrix)
+
+NVTX.@annotate function rmse(A::AbstractMatrix, B::AbstractMatrix)
     (sum((A .- B).^2) / length(A)) |> sqrt
 end
 
-NVTX.@range function doit()
+NVTX.@annotate function doit()
     rmses = Vector{eltype(A)}(undef,N)
     for i in 1:N
         rmses[i] = rmse(A[:,:,i], B[:,:,i])
@@ -21,4 +22,11 @@ NVTX.@range function doit()
 end
 
 # normal benchmark to look at initial performance.
-@benchmark doit()
+#@benchmark doit()
+doit() # warm up the GPU
+# Profile the code using nsys
+
+CUDA.@profile begin
+      doit();
+      doit();
+end
